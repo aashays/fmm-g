19ee4fd677b3da768041c37174b6fe1b003c1a50
Richard Vuduc <rvuduc@honest2.ncsa.uiuc.edu>
2009-04-23 21:28:43 -0500

    Added V-List to tune-gpu script

--- fmm3d_mpi/tune-gpu.sh
+++ fmm3d_mpi/tune-gpu.sh
@@ -4,30 +4,37 @@ OPTS_IN="$1" ; shift
 RUN_ULIST="$1" ; shift
 RUN_S2M="$1" ; shift
 RUN_L2T="$1" ; shift
-NP="$1" ; shift
+RUN_VLIST="$1" ; shift
+N="$1" ; shift
+NG="$1" ; shift
 OUTDIR="$1" ; shift
 
 if \
   test -z "${RUN_ULIST}" \
   || test -z "${RUN_S2M}" \
   || test -z "${RUN_L2T}" \
-  || test -z "${NP}" \
+  || test -z "${RUN_VLIST}" \
+  || test -z "${N}" \
+  || test -z "${NG}" \
   || test -z "${OUTDIR}" \
 ; then
   echo "
-usage: $0 <options.in> <ulist?> <s2m?> <l2t?> <num-points> <output-directory>
+usage: $0 <options.in> <ulist?> <s2m?> <l2t?> <vlist?> <num-points> <num-gpus> <output-directory>
 "
   exit 1
 fi
 
-TT0=./tt0
+TT0_BIN=./tt0${EXEEXT}
+TT0_RUN=
 OPTS_OUT_BASE=${OUTDIR}/$(basename ${OPTS_IN})
 PTSMAX_SEQ=${PTSMAX_SEQ-"32 64 128 256"}
 PTSMAX_GPU=${PTSMAX_GPU-"${PTSMAX_SEQ} 512 1024 2048 4096"}
 
-if ! mkdir -p ${OUTDIR} ; then
-  echo "*** Couldn't create output directory, '${OUTDIR}'. ***"
-  exit 1
+if ! test -d "${OUTDIR}" ; then
+  if ! mkdir -p "${OUTDIR}" ; then
+    echo "*** Couldn't create output directory, '${OUTDIR}'. ***"
+    exit 1
+  fi
 fi
 
 TAG=""
@@ -52,11 +59,25 @@ else
   GPU_L2T=""
 fi
 
+if test x"${RUN_VLIST}" = x"yes" ; then
+  GPU_VLIST="-gpu_vlist"
+  TAG="${TAG}vlist-"
+else
+  GPU_VLIST=""
+fi
+
 if test -n "${TAG}" ; then
   PTSMAX_LIST="${PTSMAX_GPU}"
 else # empty tag
   TAG="cpu-"
   PTSMAX_LIST="${PTSMAX_SEQ}"
+fi
+
+if test ${NG} -gt 1 ; then
+  NP=`wc -l ${PBS_NODEFILE} | cut -d'/' -f1`
+  MV2_SRQ_SIZE=4000
+  mvapich2-start-mpd
+  TT0_RUN="mpirun -machinefile ${PBS_NODEFILE} -np ${NP}"
 fi
 
 for PTSMAX in ${PTSMAX_LIST} ; do
@@ -67,12 +88,17 @@ for PTSMAX in ${PTSMAX_LIST} ; do
     | sed "s,@GPU_ULIST@,${GPU_ULIST},g" \
     | sed "s,@GPU_S2M@,${GPU_S2M},g" \
     | sed "s,@GPU_L2T@,${GPU_L2T},g" \
-    | sed "s,@NP@,${NP},g" \
+    | sed "s,@GPU_VLIST@,${GPU_VLIST},g" \
+    | sed "s,@N@,${N},g" \
     | sed "s,@PTSMAX@,${PTSMAX},g" \
     > ${OPTS_OUT}
-  date > ${LOG_OUT}
-  ${TT0} -options_file ${OPTS_OUT} | tee -a ${LOG_OUT}
-  date >> ${LOG_OUT}
+  date | tee ${LOG_OUT}
+  ${TT0_RUN} ${TT0_BIN} -options_file ${OPTS_OUT} 2>&1 | tee -a ${LOG_OUT}
+  date | tee -a ${LOG_OUT}
 done
+
+if test ${NG} -gt 1 ; then
+  mpdallexit
+fi
 
 # eof
